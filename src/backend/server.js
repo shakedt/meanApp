ad4b0b70b06b03e5450c94de35a386ad053115b3
shaked.tayeb <shaked.tayeb@ladbrokes.com>
2019-05-23 20:22:43 +0300

    base commit testing some concepts

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hello From Express' });
+  mongoose.connect('mongodb://shaked:163452zz@ds149606.mlab.com:49606/usermanager', {useNewUrlParser: true}).then(() => {
+    console.log('moongose connected correctly.');
+  });
+  res.send({ express: 'Hello From Express' });
 });
 app.post('/api/world', (req, res) => {
-    console.log(req.body);
-    res.send(
-        `I received your POST request. This is what you sent me: ${req.body.post}`,
-    );
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
